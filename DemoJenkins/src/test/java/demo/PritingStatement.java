82ebb329050fb155e17223a34e7bb0a0cdc17d57
Vijay mugale <Vijay mugale@hp>
2022-11-24 17:14:36 +0530

    hii

---
+++ DemoJenkins/src/test/java/demo/PritingStatement.java
@@ -0,0 +1,22 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PritingStatement {
+	
+	@Test
+	public void tc1()
+	{
+		 // Print on Console and Report
+		  Reporter.log("Sun",true);
+		  
+		  // Print on only Report
+		  Reporter.log("Mon",false);
+		  Reporter.log("wed");
+		  
+		  // Print on only Console
+		  System.out.println("Thu");
+	}
+
+}
